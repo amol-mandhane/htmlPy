6ed78aa35f45f73cb505c2fa95b33b6828fe284b
Amol Mandhane <manamol296@gmail.com>
2015-11-29 02:25:02 +0530

    Fix #4: Error in documentation

--- docs/codes/sample_web_app.py
+++ docs/codes/sample_web_app.py
@@ -1,6 +1,6 @@
 import htmlPy
 
-web_app = htmlPy.WebAppGUI(title="Python Website", maximized=True)
+web_app = htmlPy.WebAppGUI(title=u"Python Website", maximized=True)
 web_app.url = u"http://python.org/"
 
 web_app.start()
